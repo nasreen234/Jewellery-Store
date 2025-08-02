d7c43dd605c57e8d9bcc484d1a4ed7630d854c72
nasreen234 <nasreen234@gmail.com>
2025-08-02 19:57:18 +0530

    thirteenth commit

--- Controllers/Usercontroller.js
+++ Controllers/Usercontroller.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 // Generate token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1m' });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30m' });
 };
 
 // Register Admin/User
@@ -27,22 +27,22 @@ const registerUser = async (req, res) => {
 };
 
 // Login Admin/User
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
-  }
-};
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  };
 // Get All Users - Admin only
 const getAllUsers = async (req, res) => {
   try {
